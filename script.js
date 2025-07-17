178923b61b323e22815a3feb4fc9dffc2eaa2a9a
jorabekbahronov <jorabekbahronov3@gmail.com>
2025-07-17 17:16:53 +0500

    aasd

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let input = +prompt()
-let x = "chetniye chisla"
-let y = "ne chetniye chisla"
 
-if (input % 2 == 0) {
-    console.log(x)
-} else {
-    console.log(y)
-}
+// 1-masala 
+// let input = +prompt()
+// let x = "chetniye chisla"
+// let y = "ne chetniye chisla"
+
+// if (input % 2 == 0) {
+//     console.log(x)
+// } else {
+//     console.log(y)
+// }
+
 
 // 2 - masala
