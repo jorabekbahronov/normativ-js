8266a9bf5e85bac1820429e66520f186f3718ef9
jorabekbahronov <jorabekbahronov3@gmail.com>
2025-07-29 15:36:43 +0500

    sdf

--- script.js
+++ script.js
@@ -12,3 +12,68 @@
 
 
 // 2 - masala
+
+//   let input = +prompt()
+//   let x = "polojitelnoe"
+//   let y = "otritsatelnoe"
+//   let z = 0
+
+//    if (input > 0) {
+//     console.log(x)
+//    } 
+//     else if (input == 0) {
+//         console.log(z);
+//    }
+//    else {
+//     console.log(y)
+//    }
+
+
+//3 massala
+
+
+// function найтиБольшееЧисло(a, b) {
+//   if (a > b) {
+//     console.log(`x = ${a}, число a больше числа b`);
+//   } else if (b > a) {
+//     console.log(`x = ${b}, число b больше числа a`);
+//   } else {
+//     console.log("Числа a и b равны");
+//   }
+// }
+
+
+
+
+// function name(params) {
+    
+// }
+
+
+
+// 4-misol
+
+
+// function bahoVaNatija(ball) {
+//     let baho = "";
+//     let natija = "";
+
+//     if (ball >= 1 && ball <= 59) {
+//         baho = 2;
+//         natija = "ine proshol examen";
+//     } else if (ball >= 60 && ball <= 69) {
+//         baho = 3;
+//         natija = "proshol examen";
+//     } else if (ball >= 70 && ball <= 89) {
+//         baho = 4;
+//         natija = "proshol cherez examen";
+//     } else if (ball >= 90 && ball <= 100) {
+//         baho = 5;
+//         natija = "proshol cherez examen";
+//     } else {
+//         return "Notog'ri ball kiritildi";
+//     }
+
+//     console.log(baho)
+      
+// }
